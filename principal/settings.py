322b0df276f4069af249047a816163b8ac05a419
Javier Diaz <jav.diazd@duocuc.cl>
2025-05-15 00:32:10 -0400

    Fix: renombrado correcto de Procfile

--- principal/settings.py
+++ principal/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%n=me%*f2!@9rigemjwiw2iabfqqck!9!dt#we8(jyejp_$rfp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['servimatch-backend.onrender.com']
 
 
 # Application definition
@@ -44,8 +44,9 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://192.168.2.12",
+    "https://servimatch-backend.onrender.com",
 ]
+
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'principal.urls'
 
@@ -117,10 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
